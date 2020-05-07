974c50233f63f19e9ae67e4d302765a188b69fb6
HwangNinaa <63974132+HwangNinaa@users.noreply.github.com>
2020-05-07 21:48:21 +0900

    070520 JS

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 const title = document.querySelector("#title");
-title.innerHTML = "Hi from JS!";
-title.style.color = "lightgreen";
+
+const CLICKED_CLASS = "clicked";
+
+function handleClick() {
+  const hasClass = title.classList.contains(CLICKED_CLASS);
+  if (hasClass) {
+    title.classList.remove(CLICKED_CLASS);
+  } else {
+    title.classList.add(CLICKED_CLASS);
+  }
+}
+
+function init() {
+  title.addEventListener("click", handleClick);
+}
+init();
